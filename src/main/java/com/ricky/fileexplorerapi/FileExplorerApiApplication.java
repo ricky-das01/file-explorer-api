bf2af8f59a3993fa29b1f480a8462369413dc935
ricky-das01 <abhishekdasuk@gmail.com>
2026-05-23 17:54:58 +0100

    Initial Spring Boot Project setup

---
+++ src/main/java/com/ricky/fileexplorerapi/FileExplorerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ricky.fileexplorerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExplorerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileExplorerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ricky/fileexplorerapi/FileExplorerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricky.fileexplorerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExplorerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
